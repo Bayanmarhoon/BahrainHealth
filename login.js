37fdcd4412c5a9ace4399778d88502e6c9c355fe
Bayanmarhoon <114220808+Bayanmarhoon@users.noreply.github.com>
2022-11-01 15:17:19 +0300

    Update login.js

--- login.js
+++ login.js
@@ -1,11 +1,31 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
+var $loginMsg = $('.loginMsg'),
+  $login = $('.login'),
+  $signupMsg = $('.signupMsg'),
+  $signup = $('.signup'),
+  $frontbox = $('.frontbox');
 
-signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
-});
+$('#switch1').on('click', function() {
+  $loginMsg.toggleClass("visibility");
+  $frontbox.addClass("moving");
+  $signupMsg.toggleClass("visibility");
 
-signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
-}); 
+  $signup.toggleClass('hide');
+  $login.toggleClass('hide');
+})
+
+$('#switch2').on('click', function() {
+  $loginMsg.toggleClass("visibility");
+  $frontbox.removeClass("moving");
+  $signupMsg.toggleClass("visibility");
+
+  $signup.toggleClass('hide');
+  $login.toggleClass('hide');
+})
+
+setTimeout(function(){
+  $('#switch1').click()
+},1000)
+
+setTimeout(function(){
+  $('#switch2').click()
+},3000)
